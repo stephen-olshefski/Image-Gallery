c07626aef8552070c489f943aa55880ccf724e41
Stephen Olshefski <stephen.olshefski@outlook.com>
2018-07-01 19:47:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//const current = document.querySelector('#current');
+//const imgs = document.querySelectorAll('.imgs img');
+//Same as above
+const [current, imgs] = [
+    document.querySelector('#current'),
+    document.querySelectorAll('.imgs img'),
+];
+
+const opacity = 0.6;
+
+//Set first image opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => {
+    img.addEventListener('click', imgClick);
+});
+
+function imgClick(e) {
+    //Reset opacity of all images
+    imgs.forEach(img => {
+        img.style.opacity = 1;
+    });
+    //Change current image to source of clicked image
+    current.src = e.target.src;
+    //Add fade in class
+    current.classList.add('fade-in');
+    //Remove fade in class after 0.5 seconds
+    setTimeout(() => {
+        current.classList.remove('fade-in')
+    }, 500);
+    //Change the opacity to the opacuty variable
+    e.target.style.opacity = opacity;
+}
